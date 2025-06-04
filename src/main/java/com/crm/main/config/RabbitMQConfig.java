8211d0e24e79123691c0c25084e72b168814db50
Pavel Tseva <paveltseva@gmail.com>
2025-06-04 21:05:46 +0300

    Add exchange, binding and queue for future access control authorization filter.

--- src/main/java/com/crm/main/config/RabbitMQConfig.java
+++ src/main/java/com/crm/main/config/RabbitMQConfig.java
@@ -1,9 +1,6 @@
 package com.crm.main.config;
 
-import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -15,6 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static com.crm.main.constants.RabbitConstants.MAIN_SERVICE_EXCHANGER_NAME;
+import static com.crm.main.constants.RabbitConstants.ORGANIZATION_USER_ROLE_CHANGE_ROUTING_KEY;
+import static com.crm.sharedlib.consts.CrmConstants.ORGANIZATION_USER_ROLES_SYNC_QUEUE;
 import static com.crm.sharedlib.consts.CrmConstants.SEND_PASSWORD_QUEUE;
 
 @Configuration
@@ -29,6 +29,9 @@ public class RabbitMQConfig {
     private String username;
     @Value("${spring.rabbitmq.password}")
     private String password;
+
+    @Value("${app.ampq.organisation-user-sync-roles.queue.ttl}")
+    private Integer orgUserSyncRolesQueueTtl;
 
     @Bean
     public MessageConverter messageConverter() {
@@ -70,6 +73,38 @@ public class RabbitMQConfig {
         return QueueBuilder
                 .durable(SEND_PASSWORD_QUEUE)
                 .build();
+    }
+
+    @Configuration
+    public class ProducerConfig {
+
+        @Bean("mainServiceTopicExchanger")
+        public TopicExchange mainServiceTopicExchanger() {
+            return ExchangeBuilder
+                    .topicExchange(MAIN_SERVICE_EXCHANGER_NAME)
+                    .durable(true)
+                    .build();
+        }
+
+        @Bean("orgUserSyncRoles")
+        public Queue orgUserSyncRoles() {
+            return QueueBuilder
+                    .durable(ORGANIZATION_USER_ROLES_SYNC_QUEUE)
+                    .ttl(orgUserSyncRolesQueueTtl)
+                    .build();
+        }
+
+        @Bean
+        public Binding OrgUserRoleChangeBinding(
+                TopicExchange mainServiceTopicExchanger,
+                Queue orgUserSyncRoles
+        ) {
+            return BindingBuilder
+                    .bind(orgUserSyncRoles)
+                    .to(mainServiceTopicExchanger)
+                    .with(ORGANIZATION_USER_ROLE_CHANGE_ROUTING_KEY);
+        }
+
     }
 
 }
