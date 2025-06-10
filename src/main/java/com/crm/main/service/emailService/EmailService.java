03ee3d2e710ed42475c1e70670c63111e58e4441
I. Sham <ishamanskiy384@gmail.com>
2025-06-10 03:26:23 +0300

    Created class which will be sent email to users with html format, subject and text

---
+++ src/main/java/com/crm/main/service/emailService/EmailService.java
@@ -0,0 +1,41 @@
+package com.crm.main.service.emailService;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    private TemplateBuilder templateBuilder;
+
+    public void sendEmail(String to, String text, String subject) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = null;
+
+        try {
+            helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setSubject(subject);
+            helper.setTo(to);
+            helper.setText(templateBuilder.buildPasswordEmail(text), true);
+        } catch (MessagingException e) {
+            log.error("Something went wrong while sending email", e);
+            throw new RuntimeException(e);
+        }
+
+        mailSender.send(message);
+
+        log.info("Email successfully sent");
+    }
+}
