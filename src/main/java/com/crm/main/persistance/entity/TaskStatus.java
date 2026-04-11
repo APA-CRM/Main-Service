1c7c8c429ae6aaedb996e1dcd5215342a8324b21
Pavel Tseva <paveltseva@gmail.com>
2026-04-11 15:52:13 +0300

    Introduce TaskStatus entity

---
+++ src/main/java/com/crm/main/persistance/entity/TaskStatus.java
@@ -0,0 +1,47 @@
+package com.crm.main.persistance.entity;
+
+import com.crm.main.enums.TaskStatusType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(indexes = @Index(name = "task_status_organization_id_idx", columnList = "organization_id"))
+public class TaskStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatusType statusType;
+
+    @ManyToOne(optional = false)
+    private Organization organization;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+
+}
