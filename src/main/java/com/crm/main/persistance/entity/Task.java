fe627516aacfaa9e7468c6a965affb03be3ac10d
Pavel Tseva <paveltseva@gmail.com>
2026-04-13 13:36:03 +0300

    Introduce Task entity

---
+++ src/main/java/com/crm/main/persistance/entity/Task.java
@@ -0,0 +1,66 @@
+package com.crm.main.persistance.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(indexes = {
+        @Index(name = "task_organization_id_idx", columnList = "organization_id"),
+        @Index(name = "task_status_id_idx", columnList = "status_id"),
+        @Index(name = "task_priority_id_idx", columnList = "priority_id"),
+        @Index(name = "task_assigned_to_idx", columnList = "assigned_to"),
+        @Index(name = "task_created_by_idx", columnList = "created_by"),
+})
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    private Integer estimatedTime;
+
+    @ManyToOne
+    private Organization organization;
+
+    @ManyToOne
+    private TaskStatus status;
+
+    @ManyToOne
+    private TaskPriority priority;
+
+    private Long assignedTo;
+
+    private Long createdBy;
+
+    private Instant dueDate;
+
+    private Instant reminderAt;
+
+    private Instant completedAt;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+
+}
